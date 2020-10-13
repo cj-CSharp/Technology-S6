a71e2e4bd86f817861b0396ff08da03c57dad5d3
cjconnor18 <cjallen1988@gmail.com>
2020-10-13 15:53:20 -0500

    added properties to Computer

--- Technology/Computer.cs
+++ Technology/Computer.cs
@@ -4,7 +4,23 @@ using System.Text;
 
 namespace Technology
 {
-    class Computer
+    class Computer //Parent Class
     {
+        public string OperatingSystem { get; set; }
+        public int HardDriveSize { get; set; }
+        public bool HasTouchScreen { get; set; }
+
+        public Computer (string operatingSystem, int harddriveSize, bool hasTouchScreen)
+        {
+            OperatingSystem = operatingSystem;
+            HardDriveSize = harddriveSize;
+            HasTouchScreen = hasTouchScreen;
+        }
+
+        public Computer() : this("", 0, false) { }
+
+
+
+
     }
 }
--- Technology/Laptop.cs
+++ Technology/Laptop.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Technology
 {
-    class Laptop
+    class Laptop //child class
     {
     }
 }
