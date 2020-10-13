7b2b799cd518bbf177ef4839dcefc3e2cd22e14d
cjconnor18 <cjallen1988@gmail.com>
2020-10-13 16:15:20 -0500

    Laptop methods constructor and fields

--- Technology/Laptop.cs
+++ Technology/Laptop.cs
@@ -6,5 +6,20 @@ namespace Technology
 {
     class Laptop : Computer //child class
     {
+        public double Weight { get; }
+
+        public Laptop(string operatingSystem, int harddriveSize, bool hasTouchScreen, bool isOn, double weight) : base(operatingSystem, harddriveSize, hasTouchScreen, isOn)
+        {
+            Weight = weight;
+        }
+
+        public bool IsHeavy()
+        {
+            if(Weight > 6)
+            {
+                return true;
+            }
+            return false;
+        }
     }
 }
